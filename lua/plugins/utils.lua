28d1581f7c8b731e183164a27d6948c369c0af85
Sang Cao <hoaisang0806@gmail.com>
2025-11-03 19:12:22 +0700

    feat: add config for vimtex

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -99,7 +99,21 @@ return {
 		-- tag = "v2.15", -- uncomment to pin to a specific release
 		init = function()
 			-- VimTeX configuration goes here, e.g.
-			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_view_method = "mupdf"
+			vim.g.vimtex_compiler_latexmk = {
+				aux_dir = "",
+				out_dir = "build",
+				callback = 1,
+				continuous = 1,
+				executable = "latexmk",
+				hooks = {},
+				options = {
+					"-verbose",
+					"-file-line-error",
+					"-synctex=1",
+					"-interaction=nonstopmode",
+				},
+			}
 		end,
 	},
 }
