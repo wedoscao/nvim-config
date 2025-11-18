25d3c1733656ab10dad3a120ab1f016cfff9f588
Sang Cao <hoaisang0806@gmai.com>
2025-11-18 16:26:56 +0700

    feat: remove live preview and vimtex

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -79,46 +79,4 @@ return {
 			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 		},
 	},
-
-	{
-		"brianhuster/live-preview.nvim",
-		dependencies = {
-			-- You can choose one of the following pickers
-			"nvim-telescope/telescope.nvim",
-			"ibhagwan/fzf-lua",
-			"echasnovski/mini.pick",
-			"folke/snacks.nvim",
-		},
-		config = function()
-			require("livepreview.config").set()
-		end,
-	},
-	{
-		"lervag/vimtex",
-		lazy = false, -- we don't want to lazy load VimTeX
-		-- tag = "v2.15", -- uncomment to pin to a specific release
-		init = function()
-			-- VimTeX configuration goes here, e.g.
-			vim.g.vimtex_view_method = "mupdf"
-			vim.g.vimtex_compiler_latexmk = {
-				aux_dir = "",
-				out_dir = "build",
-				callback = 1,
-				continuous = 1,
-				executable = "latexmk",
-				hooks = {},
-				options = {
-					"-file-line-error",
-					"-synctex=1",
-					"-interaction=nonstopmode",
-					"-verbose",
-				},
-			}
-			vim.g.vimtex_syntax_enabled = 0
-			vim.g.vimtex_quickfix_ignore_log = 1
-			vim.g.vimtex_quickfix_mode = 0
-			vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-			vim.g.vimtex_view_forward_search_on_start = 0
-		end,
-	},
 }
