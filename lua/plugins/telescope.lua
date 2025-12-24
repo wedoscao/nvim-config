f2a799f70f02866dca3112db48f5b8003a8a706a
Hoai Sang Cao <hoaisang0806@gmail.com>
2025-12-24 13:37:44 +0700

    feat: fix treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
+	branch = "master",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local arg = vim.fn.argv()[1]
