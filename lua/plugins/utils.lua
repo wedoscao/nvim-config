49ca430c9aae953d8721332c6cc7eff2afc97e41
Hoai Sang Cao <hoaisang0806@gmail.com>
2025-12-29 19:01:37 +0700

    feat: install transparent background plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -79,4 +79,10 @@ return {
 			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 		},
 	},
+
+	{
+		"tribela/transparent.nvim",
+		event = "VimEnter",
+		config = true,
+	},
 }
